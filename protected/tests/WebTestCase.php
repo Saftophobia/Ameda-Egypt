045ee6e361327389ccb68b31208976baf4d10aac
cena <mk.cena92@gmail.com>
2012-08-31 20:19:13 +0200

    removing unnecessary files

--- protected/tests/WebTestCase.php
+++ protected/tests/WebTestCase.php
@@ -17,11 +17,8 @@ class WebTestCase extends CWebTestCase
 	 * Sets up before each test method runs.
 	 * This mainly sets the base URL for the test application.
 	 */
-<<<<<<< HEAD
-	protected function setUp()
-=======
+
 	public function setUp()
->>>>>>> ec53b71d62f4c9444cf825f9146d773cc5d14778
 	{
 		parent::setUp();
 		$this->setBrowserUrl(TEST_BASE_URL);
