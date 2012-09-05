febb71f23fce5f7597013860817f9d4514e0e26d
Saftophobia <asafty2003@gmail.com>
2012-09-05 21:29:34 +0200

    Changed homepage view because i want to

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,20 +1,20 @@
 <?php
 /* @var $this SiteController */
 
-$this->pageTitle=Yii::app()->name;
+$this->pageTitle="Ameda Egypt";
 ?>
 
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
+<h1>AMEDA...</h1>
 
-<p>Congratulations! You have successfully created your Yii application.</p>
+<p>Ameda breastfeeding products have been serving the needs of Mothers and babies for well over 50 years.</p>
 
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <code><?php echo __FILE__; ?></code></li>
-	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
-</ul>
+<p>We know how much breastfeeding matters to you and your baby.</p>
 
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+<p>Our website aims to provide education and information for breastfeeding Mothers and to support them in their efforts to provide precious breast milk to their babies for as long as they wish.  At times this may require the assistance of a quality breast pump or breastfeeding accessory and these can be found within this website.</p>
+
+<p>If you have been referred to this site you can now go to the On-Line shop to order your pump or accessory.</p>
+
+
+
+
+<p>If you are not sure of the product you require please access the <a href="index.php?r=Product"> Product List</a>.</p>
