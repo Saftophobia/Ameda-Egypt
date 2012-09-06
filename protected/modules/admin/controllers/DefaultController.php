e3e74c97c2f7ff1fcaaf89e917049165b5c379ed
cena <mk.cena92@gmail.com>
2012-09-06 06:37:18 +0200

    added authorization to the admin system access

--- protected/modules/admin/controllers/DefaultController.php
+++ protected/modules/admin/controllers/DefaultController.php
@@ -4,6 +4,10 @@ class DefaultController extends Controller
 {
 	public function actionIndex()
 	{
+		if(!Yii::app()->user->checkAccess('admin'))
+		{
+			throw new CHttpException(403,'You are not authorized to perform this action');
+		}
 		$this->render('index');
 	}
 
