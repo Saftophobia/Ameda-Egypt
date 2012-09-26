85067d4495d7466c4918e11d4c3a064eb1dae4ec
Saftophobia <asafty2003@gmail.com>
2012-09-26 09:36:19 +0200

    added stores link to homepage

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -36,6 +36,7 @@
 				            'visible'=>!Yii::app()->user->checkAccess('admin')),
 				array('label'=>'Products', 'url'=>array('/product/index')),
 				array('label'=>'Forum', 'url'=>array('/category/index')),
+				array('label'=>'Stores', 'url'=>array('/stores/index')),
 				array('label'=>'Register', 'url'=>array('/user/create'),
 				                'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Admin', 'url'=>array('/admin'),
