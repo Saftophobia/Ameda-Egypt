dcb69435d1cbd06adc8aa044adccd60757b8e2ca
cena <mk.cena92@gmail.com>
2012-10-15 09:14:55 +0200

    users image aligned center above username

--- protected/views/comment/commentPartial.php
+++ protected/views/comment/commentPartial.php
@@ -8,18 +8,18 @@ $user=User::model()->findByPk($data[$i]->user_id);
 <caption style="text-align:left;background-color: #00547A;color:white;">
 <?php echo $data[$i]->created_at;?>
 </caption>
-<tr>
-<td style="background-color:#e9e9e9 ;vertical-align:top;width: 100px; height: 150px;">
-<div style="font-size: 30px; font-weight:bold; ">
+<tr >
+<td style="word-wrap:break-word;max-width:200px;min-width:200px;background-color:#e9e9e9 ;vertical-align:top;width: 200px; height: 150px;">
+<div style='color: black;font-size:12px;text-align: left;'>
+<div style="text-align:center;margin-top:20px">
+<?php echo CHtml::image($user->getFileUrl('normal'),"User's image "); ?>
+</div>
+<br/ >
+<div style="font-size: 30px; font-weight:bold;text-align:center ">
 <?php echo CHtml::link($user->username,array('/user/view','id'=>$data[$i]->user_id)); ?>
 </div>
 <br/>
 <br/>
-<div style='color: black;font-size:12px;text-align: left'>
-
-<?php echo CHtml::image($user->getFileUrl('normal'),"User's image "); ?>
-<br/ >
-<br/ >
 Name: <?php echo $user->first_name;?> <?php echo $user->last_name;?>
 
 <br/ >
@@ -32,7 +32,7 @@ Date Joined: <?php echo $user->date_joined;?>
 <br/ >
 </div>
 </td>
-<td style="vertical-align:top;text-align:left;color:black;width: 300px; height: 150px;">
+<td style="word-wrap:break-word;max-width:400px;min-width:400px;vertical-align:top;text-align:left;color:black;width: 400px; height: 150px;">
 <hr />
 <div style="font-size:25px;">
 <?php echo $data[$i]->content; ?>
